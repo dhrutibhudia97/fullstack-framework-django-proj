7449c310971dd7f9aa64fb3ff15c19047e02a127
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-02-17 16:40:05 +0000

    rendering data 1

--- todo/models.py
+++ todo/models.py
@@ -3,15 +3,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    #null=false prevents items without a name being created.
-    # blank=false makes the field required on forms.   
+    # null=false prevents items without a name being created.
+    # blank=false makes the field required on forms.
     done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
         return self.name
-        #returns item classes name attribute (name put into form)
-
-
-
+        # returns item classes name attribute (name put into form)
